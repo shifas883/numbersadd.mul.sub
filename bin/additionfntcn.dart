f9277837ae3590f6891ae0f3202a1fe9438537e7
shifas <shifasmeharnk@gmail.com>
2021-11-15 02:08:14 +0530

    input two or three numbers and add/sub/mul

---
+++ bin/additionfntcn.dart
@@ -0,0 +1,24 @@
+void main() {
+//   var result=add(7,7);
+//
+//   print(result);
+// }
+// add(int num1,int num2,[int num3=0]) {
+//   return num1 + num2 + num3;
+// }
+//
+// var result=substract(5,5,2);
+//
+// print(result);
+// }
+// substract(int num1,int num2,[int num3=0]) {
+//   return num1 - num2 - num3;
+
+  var result=multiple(5,2,1);
+
+  print(result);
+}
+multiple(int num1,int num2,[int num3=0]) {
+  return num1 * num2 * num3;
+
+}
